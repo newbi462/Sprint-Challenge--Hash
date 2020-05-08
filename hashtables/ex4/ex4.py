4f74602e7ff5a44e0ca7ebed3ffd1386440b4f46
newbi462 <isthisemailfree@gmail.com>
2020-05-08 12:55:24 -0400

    EX4 in progress | duplikit result

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,7 +3,35 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
+    speed = {}
+    result = []
+    for x in a:
+        key_neg = "neg" + str(x)
+        key_pos = "neg" + str(x)
 
+        if x < 0:
+            try:
+                hold_test = speed[key_neg]
+            except KeyError:
+                speed[key_neg] = True
+        else:
+            try:
+                hold_test = speed[key_pos]
+            except KeyError:
+                speed[key_pos] = True
+
+        try:
+            if speed[key_pos] is True and speed[key_neg] is True:
+                try:
+                    if speed["result" + str(x)] is True:
+                        pass
+                except KeyError:
+                    result.append(abs(x))
+                    speed["result" + str(x)] = True
+        except KeyError:
+            pass
+
+    print(speed)
     return result
 
 
